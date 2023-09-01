9151cb9195f5b6976107b88f7aa598700f000154
Филин Вячеслав Викторович <VFilin_krok@inno.tech>
2023-09-01 09:48:02 +0300

    Initial commit

---
+++ src/main/java/com/filin/venera/VeneraApplication.java
@@ -0,0 +1,13 @@
+package com.filin.venera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeneraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeneraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filin/venera/VeneraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filin.venera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeneraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
